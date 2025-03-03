c3c4329b2e58c151ff0c4e26dd123cc66bc3f6b1
KaranYadav11 <11yadavkaran@gmail.com>
2025-03-04 00:49:07 +0530

    Changes in How To

--- src/components/ApplyButton.jsx
+++ src/components/ApplyButton.jsx
@@ -9,10 +9,10 @@ const ApplyButton = () => {
   return (
     <button
       onClick={handleClick}
-      className="flex apply cursor-pointer absolute max-h-[67px] h-[3.4vw] items-center justify-center left-0 bottom-0  gap-2 text-[#0E1E14] text-[1vw] font-bold "
+      className="flex apply cursor-pointer absolute h-[44px] items-center justify-center left-0 bottom-0  gap-[16px] text-[#0E1E14] text-[16px] font-bold "
     >
       Apply Now
-      <ArrowRight className="w-5 h-5 text-[#17C95F]" />
+      <ArrowRight className="size-[24px] text-[#17C95F]" />
     </button>
   );
 };
--- src/sections/HowTo.jsx
+++ src/sections/HowTo.jsx
@@ -3,96 +3,91 @@ import visa from "../assets/images/visa.png";
 import debt from "../assets/images/debt.png";
 import coins from "../assets/images/coins.png";
 import ApplyButton from "../components/ApplyButton";
+import GetStarted from "../components/GetStarted";
 
 function HowTo() {
   return (
-    <section className="max-w-[1917px] bg-[#F6FEF7]  w-full flex items-center justify-center h-[111.8vw] max-h-[2148px] px-[13.26vw] py-[6.3vw]">
-      <div className="max-w-[1413px] max-h-[1908px] w-full h-full">
-        <div className="w-full max-h-[72px] h-[3.75vw]">
-          <h2 className="text-[#0E1E14] font-bold text-center tracking-[-4%] text-[3.33vw] leading-[3.75vw]">
-            How to pay off cards{" "}
-            <span className="leading-[3.75vw] font-normal">with Bright?</span>
-          </h2>
-        </div>
-        <div className="h-[2.6px] max-w-[1213px] w-[63.1vw] mx-auto bg-[#BAE8CB] my-[3.75vw] "></div>
-        <section className="w-full max-h-[468px] h-[24.3vw]  flex items-center justify-center gap-[8.3vw]">
-          <div className="max-w-[441px] max-h-[468px] h-[24.3vw] w-[23vw] bg-white rounded-[1.38vw] flex items-center justify-center">
-            <img src={phone} alt="phone" />
-          </div>
-          <div className="max-w-[645px] max-h-[366px]  relative h-[19vw] w-[33.5vw]  flex flex-col items-center justify-between">
-            <div className="relative h-[19vw] w-[33.5vw]  flex items-center justify-center ">
-              <div className="text-[#17C95F] font-bold text-[1.25vw] absolute top-[-8px] left-0">
-                01
-              </div>
-              <div className="absolute top-[3vw] w-full h-full flex flex-col items-start justify-start space-y-[1.6vw] ">
-                <p className="max-h-[91vw] leading-[3.17vw] tracking-[-2%] text-[#383C43] font-bold text-[2.18vw] h-[4.7vw]">
-                  Download the App and check the rates.
-                </p>
-                <p className="max-h-[68vw] w-full leading-[2.39vw] tracking-[-2%] text-[##787F88]  text-[1.6vw] h-[3.5vw]">
-                  Get the best rates and credit limit with a quick soft credit
-                  inquiry.
-                </p>
-              </div>
-            </div>
-            <ApplyButton />
-          </div>
-        </section>
-        <div className="h-[2.6px] max-w-[1213px] w-[63.1vw] mx-auto bg-[#BAE8CB] my-[3.73vw]"></div>
-        <section className="w-full max-h-[468px] h-[24.3vw]  flex items-center justify-center gap-[8.3vw]">
-          <div className="max-w-[645px] max-h-[366px]  relative h-[19vw] w-[33.5vw]  flex flex-col items-center justify-between">
-            <div className="relative h-[19vw] w-[33.5vw]  flex items-center justify-center ">
-              <div className="text-[#17C95F] font-bold text-[1.25vw] absolute top-[-8px] left-0">
-                02
-              </div>
-              <div className="absolute top-[3vw] w-full h-full flex flex-col items-start justify-start space-y-[1.6vw] ">
-                <p className="max-h-[91vw] leading-[3.17vw] tracking-[-2%] text-[#383C43] font-bold text-[2.18vw] h-[4.7vw]">
-                  Get the credit line and choose which cards to pay.
-                </p>
-                <p className="max-h-[68vw] w-full leading-[2.39vw] tracking-[-2%] text-[##787F88]  text-[1.6vw] h-[3.5vw]">
-                  Refinance multiple cards up to your credit limit pay down and
-                  choose more.
-                </p>
-              </div>
-            </div>
-            <ApplyButton />
-          </div>
-          <div className="max-w-[441px] max-h-[468px] h-[24.3vw] w-[23vw] bg-white rounded-[1.38vw] flex items-center justify-center">
-            <img src={visa} alt="visa" />
-          </div>
-        </section>
-        <div className="h-[2.6px] max-w-[1213px] w-[63.1vw] mx-auto bg-[#BAE8CB] my-[3.75vw]"></div>
+    <section className="w-full bg-[#F6FEF7]  h-[1592px]   px-[32px] py-[60px]">
+      <div className="h-[1472px]  w-full ">
+        <h2 className="text-[#030303] font-bold text-center tracking-[-1px] text-[32px] leading-[38px]">
+          How to pay off cards{" "}
+          <span className=" font-normal">with Bright?</span>
+        </h2>
+        <hr className="h-[2px] mx-auto text-[#F4F5F8] w-[296px] my-[31px]" />
 
-        <section className="w-full max-h-[468px] h-[24.3vw]  flex items-center justify-center gap-[8.3vw]">
-          <div className="max-w-[441px] relative max-h-[468px] h-[24.3vw] w-[23vw] bg-white rounded-[1.38vw] flex items-center justify-center">
+        <section className="w-full   h-[368px] flex flex-col items-center justify-center gap-[24px]">
+          <div className="w-full  h-[192px] gap-[10px]  relative flex flex-col items-start  justify-start">
+            <div className="text-[#17C95F] font-bold leading-[24px] text-[16px]  top-[-6px] w-[16px] h-[24px] left-0">
+              01
+            </div>
+            <p className="h-[56px] w-fit leading-[28px] text[#161414] font-bold text-[18px] ">
+              Download the App and check the rates.
+            </p>
+            <p className="h-[38px] w-fit leading-[26.66px] text[#787F88] font-normal text-[16px] tracking-[-2%] ">
+              Get the best rates and credit limit with a quick soft credit
+              inquiry.
+            </p>
+
+            <ApplyButton />
+          </div>
+          <div className="w-full h-[152px] flex items-center justify-center bg-white rounded-[26.67px] shadow-[0px_16px_53.33px_0px_#00000014]">
+            <img
+              src={phone}
+              alt="phone"
+              className="w-auto object-cover h-[152px]"
+            />
+          </div>
+        </section>
+        <hr className="h-[2px] mx-auto text-[#F4F5F8] w-[296px] my-[31px]" />
+        <section className="w-full  h-[368px] flex flex-col items-center justify-center gap-[24px]">
+          <div className="w-full h-[192px] gap-[10px] relative flex flex-col items-start justify-start">
+            <div className="text-[#17C95F] font-bold leading-[24px] text-[16px]  top-[-6px] w-[16px] h-[24px] left-0">
+              02
+            </div>
+            <p className="h-[56px] w-fit leading-[28px] text-[#161414] font-bold text-[18px]">
+              Get the credit line and choose which cards to pay.
+            </p>
+            <p className="h-[38px] w-fit leading-[26.66px] text-[#787F88] font-normal text-[16px] tracking-[-2%]">
+              Refinance multiple cards up to your credit limit pay down and
+              choose more.
+            </p>
+            <ApplyButton />
+          </div>
+          <div className="w-full h-[152px] flex items-center justify-center bg-white rounded-[26.67px] shadow-[0px_16px_53.33px_0px_#00000014]">
+            <img
+              src={visa}
+              alt="visa"
+              className="w-auto object-cover h-[152px]"
+            />
+          </div>
+        </section>
+        <hr className="h-[2px] mx-auto text-[#F4F5F8] w-[296px] my-[31px]" />
+        <section className="w-full h-[368px] flex flex-col items-center justify-center gap-[24px]">
+          <div className="w-full  h-[192px] gap-[10px] relative flex flex-col items-start justify-start">
+            <div className="text-[#17C95F] font-bold leading-[24px] text-[16px]  top-[-6px] w-[16px] h-[24px] left-0">
+              03
+            </div>
+            <p className="h-[56px] w-fit leading-[28px] text-[#161414] font-bold text-[18px]">
+              Make on-time payments and defeat debt.
+            </p>
+            <p className="h-[38px] w-fit leading-[26.66px] text-[#787F88] font-normal text-[16px] tracking-[-2%]">
+              Choose how much to pay each month above your monthly minimum
+              payment.
+            </p>
+            <ApplyButton />
+          </div>
+          <div className="w-full h-[152px] flex items-center justify-center bg-white rounded-[26.67px] shadow-[0px_16px_53.33px_0px_#00000014]">
             <img
               src={debt}
               alt="debt"
-              className="max-w-[242px] h-auto w-[12.6vw]"
+              className="w-auto object-cover h-[152px]"
             />
-            <img
-              src={coins}
-              alt="coins"
-              className="max-w-[164px] absolute top-[11.5vw] left-[11.9vw] h-auto w-[8.54vw]"
-            />
-          </div>
-          <div className="max-w-[645px] max-h-[366px]  relative h-[19vw] w-[33.5vw]  flex flex-col items-center justify-between">
-            <div className="relative h-[19vw] w-[33.5vw]  flex items-center justify-center ">
-              <div className="text-[#17C95F] font-bold text-[1.25vw] absolute top-[-8px] left-0">
-                03
-              </div>
-              <div className="absolute top-[3vw] w-full h-full flex flex-col items-start justify-start space-y-[1.6vw] ">
-                <p className="max-h-[91vw] leading-[3.17vw] tracking-[-2%] text-[#383C43] font-bold text-[2.18vw] h-[4.7vw]">
-                  Make on-time payments and defeat debt.
-                </p>
-                <p className="max-h-[68vw] w-full leading-[2.39vw] tracking-[-2%] text-[##787F88]  text-[1.6vw] h-[3.5vw]">
-                  Choose how much to pay each month above your monthly minimum
-                  payment.
-                </p>
-              </div>
-            </div>
-            <ApplyButton />
           </div>
         </section>
+
+        <div className="mt-[32px] flex items-center justify-center">
+          <GetStarted />
+        </div>
       </div>
     </section>
   );
